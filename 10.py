ccd8c7fe199d72a16e5344771404ad271f3bbd93
Aliona Matweyenko <matveenkoalena2844@mail.ru>
2023-07-11 14:51:08 +0300

    Add fuction reading csv file

--- 10.py
+++ 10.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from abc import ABC, abstractmethod
+import csv
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+CellValue = str | int | float
 
+class TableReader(ABC):
+    @abstractmethod
+    def __init__(self, filename: str):
+        pass
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    @abstractmethod
+    def read(self) -> list[list[CellValue]]:
+        pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class CSVReader(TableReader):
+    def __init__(self, filename: str):
+        self._filename = filename
+
+    def read(self) -> list[list[CellValue]]:
+        info_list: list[list[CellValue]] = []
+        with open(self._filename, 'r') as fd:
+            file_reader = csv.reader(fd, delimiter=',')
+            for row in file_reader:
+                info_list.append(row)
+        print(info_list)
+        return info_list
+
+if __name__ == "__main__":
+    content = [
+        ['Столбец1', 'Столбец2'],
+        [0, 1],
+        [2, 3],
+    ]
+    reader = CSVReader('table_csv.csv')
+    reader.read()
